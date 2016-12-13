bc53ffb82fa8f27462a934d70286ab95f61c51f7
drceogodpope <juventini_soccer10@hotmail.com>
2016-12-13 18:29:23 -0500

    Initial commit

---
+++ app/src/main/java/com/heapdragon/lots/MainActivity.java
@@ -0,0 +1,59 @@
+package com.heapdragon.lots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSites();
+
+    }
+
+
+    public void addSite(String name,int numberOfLots){
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference rootRef = database.getReference();
+        DatabaseReference sites_infoRef = rootRef.child("Sites_info").push();
+        sites_infoRef.child("name").setValue(name);
+        sites_infoRef.child("total_lots").setValue(numberOfLots);
+    }
+
+    public void getSites() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference rootRef = database.getReference();
+        DatabaseReference sites_infoRef = rootRef.child("Sites_info");
+        sites_infoRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "Something Changed Again");
+                for(DataSnapshot child:dataSnapshot.getChildren()){
+                    Log.d(TAG,child.getKey() + " : " + child.child("name").getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+}
