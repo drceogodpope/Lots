c93d9a783f5d1f4928c83044275be26f35f685bc
drceogodpope <juventini_soccer10@hotmail.com>
2016-12-20 17:30:41 -0500

    wrote randum cusses

--- app/src/main/java/com/heapdragon/lots/MainActivity.java
+++ app/src/main/java/com/heapdragon/lots/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                String tits = "Shitty tits";
             }
         });
     }
