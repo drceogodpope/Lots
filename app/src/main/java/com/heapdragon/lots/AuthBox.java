19a10b98964f4c8b39920031fac7ff93cc53c06e
drceogodpope <juventini_soccer10@hotmail.com>
2017-04-08 18:49:51 -0400

    Authbox works

--- app/src/main/java/com/heapdragon/lots/AuthBox.java
+++ app/src/main/java/com/heapdragon/lots/AuthBox.java
@@ -1,8 +1,53 @@
 package com.heapdragon.lots;
 
-/**
- * Created by Francesco on 2017-04-03.
- */
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
 
-public class AuthBox {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+ class AuthBox {
+    private FirebaseAuth auth;
+    private Context context;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    public AuthBox(final Context context){
+        auth = FirebaseAuth.getInstance();
+        this.context = context;
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(auth.getCurrentUser() == null){
+                    startMainActivity();
+                }
+            }
+        };
+    }
+
+    void startSignIn(String username,String password){
+        auth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(context, "Incorrect Email/Password!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    startMainActivity();
+                }
+            }
+        });
+    }
+
+    private void startMainActivity(){
+        context.startActivity(new Intent(context,MainActivity.class));
+    }
+
+    void setOnStateListener(){
+        auth.addAuthStateListener(authStateListener);
+    }
 }
--- app/src/main/java/com/heapdragon/lots/LoginActivity.java
+++ app/src/main/java/com/heapdragon/lots/LoginActivity.java
@@ -1,119 +1,50 @@
 package com.heapdragon.lots;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
-    private TextView tv;
-    private EditText et;
+public class LoginActivity extends AppCompatActivity{
+    private TextView usernameField;
+    private EditText passwordField;
+    private AuthBox authBox;
+    private Button loginButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        tv = (TextView) findViewById(R.id.login_textView);
-        et = (EditText) findViewById(R.id.login_activity_editText);
-        setEnterID();
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-    }
-
-    View.OnKeyListener checkIdListener = new View.OnKeyListener() {
-        @Override
-        public boolean onKey(View view, int i, KeyEvent keyEvent) {
-            if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
-                switch (i) {
-                    case KeyEvent.KEYCODE_ENTER:
-                        if (checkId()) {
-                            setEnterPassword();
-                            return true;
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Incorrect identification!", Toast.LENGTH_SHORT).show();
-                            setEnterID();
-                        }
-                        return true;
-                    default:
-                        break;
+        authBox = new AuthBox(this);
+        usernameField = (EditText) findViewById(R.id.username_field);
+        passwordField = (EditText) findViewById(R.id.password_field);
+        loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameField.getText().toString().trim();
+                String password = passwordField.getText().toString().trim();
+                if(!(TextUtils.isEmpty(username)||TextUtils.isEmpty(password))){
+                    authBox.startSignIn(username,password);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Check Fields!",Toast.LENGTH_LONG).show();
                 }
             }
-            return false;
-        }
-    };
-
-    View.OnKeyListener checkPasswordListener = new View.OnKeyListener() {
-        @Override
-        public boolean onKey(View view, int i, KeyEvent keyEvent) {
-            if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
-                switch (i) {
-                    case KeyEvent.KEYCODE_ENTER:
-                        if (checkPassword()) {
-                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Incorrect password!", Toast.LENGTH_SHORT).show();
-                            setEnterPassword();
-                        }
-                        return true;
-                    default:
-                        break;
-                }
-            }
-            return false;
-        }
-    };
-
-
-
-    private void setEnterID() {
-        tv.setText("Enter user identification");
-        et.setText("");
-        et.setOnKeyListener(checkIdListener);
-        et.requestFocus();
-        et.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputMethodManager.showSoftInput(et, InputMethodManager.SHOW_IMPLICIT);
-            }
-        }, 1);
+        });
 
     }
-
-    private void setEnterPassword() {
-        tv.setText("Enter password");
-        et.setText("");
-        et.setOnKeyListener(checkPasswordListener);
-        et.requestFocus();
-        et.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputMethodManager.showSoftInput(et, InputMethodManager.SHOW_IMPLICIT);
-            }
-        }, 1);
-    }
-
-    private boolean checkId() {
-        //Firebase nonesense//
-        return true;
-    }
-    private boolean checkPassword() {
-        //Firebase nonsense//
-        return true;
-    }
-
 
     @Override
-    public void onBackPressed() {
-        setEnterID();
+    protected void onStart() {
+        super.onStart();
+        authBox.setOnStateListener();
     }
+
+
 }
